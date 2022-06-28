ee73ba02f8bb5108d584f4c48646678f1ea9f732
andhika <ssavanakw@gmail.com>
2022-06-28 12:30:25 +0700

    Adding Sound on each unit range
    - add sound
    - highlight last score

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class ScoreController : MonoBehaviour
 {
+    [Header("Score Highlight")]
+    public int scoreHighlightRange;
+    public CharacterSoundController sound;
+    private int lastScoreHighlight = 0;
+
     private int currentScore = 0;
     private void Start()
     {
         // reset
         currentScore = 0;
+        lastScoreHighlight = 0;
     }
     public float GetCurrentScore()
     {
@@ -17,6 +23,12 @@ public class ScoreController : MonoBehaviour
     public void IncreaseCurrentScore(int increment)
     {
         currentScore += increment;
+
+        if (currentScore - lastScoreHighlight > scoreHighlightRange)
+        {
+            sound.PlayScoreHighlight();
+            lastScoreHighlight += scoreHighlightRange;
+        }
     }
     public void FinishScoring()
     {
